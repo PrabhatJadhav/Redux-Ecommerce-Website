d9f87701f7a2359412df8946556f78791b756a77
Prabhat Jadhav <prabhatjadhav.exe@gmail.com>
2022-06-29 11:15:34 +0530

    Error page improved

--- src/Components/ErrorPage.js
+++ src/Components/ErrorPage.js
@@ -9,16 +9,14 @@ function ErrorPage() {
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
       transition={{ delay: 0.2, duration: 1 }}
-      className="height bg-black border-btm pt-5"
+      className=""
     >
-      <div className="error-pg">
-        <h1 className="color-white">Error 404</h1>
-        <h2 className="color-white">Page Not Found</h2>
-        <p>
-          <Link className="" to="/home">
-            Go To Home Page
-          </Link>
-        </p>
+      <div className="height error-pg">
+        <h1>Error 404</h1>
+        <h2>Page Not Found</h2>
+        <Link className="" to="/home">
+          Go To Home Page
+        </Link>
       </div>
     </motion.div>
   );
